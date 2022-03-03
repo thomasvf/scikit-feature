12c7a9bfe66d05b0fa8d55ae9ed25e2d74815c90
Lucas Melo <lucasmelouni@outlook.com>
2022-03-03 13:20:31 -0300

    Update t_score.py

--- skfeature/function/statistical_based/t_score.py
+++ skfeature/function/statistical_based/t_score.py
@@ -1,18 +1,25 @@
+import pandas as pd
+
+from scipy import stats
+
+from sklearn.preprocessing import PowerTransformer
+from sklearn.base import BaseEstimator, MetaEstimatorMixin
+from sklearn.feature_selection import SelectorMixin
+
 import numpy as np
+from joblib import Memory
 
 
 def t_score(X, y):
     """
     This function calculates t_score for each feature, where t_score is only used for binary problem
     t_score = |mean1-mean2|/sqrt(((std1^2)/n1)+((std2^2)/n2)))
-
     Input
     -----
     X: {numpy array}, shape (n_samples, n_features)
         input data
     y: {numpy array}, shape (n_samples,)
         input class labels
-
     Output
     ------
     F: {numpy array}, shape (n_features,)
@@ -22,6 +29,8 @@ def t_score(X, y):
     n_samples, n_features = X.shape
     F = np.zeros(n_features)
     c = np.unique(y)
+    pt = PowerTransformer()
+    X = pt.fit_transform(X)
     if len(c) == 2:
         for i in range(n_features):
             f = X[:, i]
@@ -29,16 +38,7 @@ def t_score(X, y):
             # class1 contains instances belonging to the second class
             class0 = f[y == c[0]]
             class1 = f[y == c[1]]
-            mean0 = np.mean(class0)
-            mean1 = np.mean(class1)
-            std0 = np.std(class0)
-            std1 = np.std(class1)
-            n0 = len(class0)
-            n1 = len(class1)
-            t = mean0 - mean1
-            t0 = np.true_divide(std0**2, n0)
-            t1 = np.true_divide(std1**2, n1)
-            F[i] = np.true_divide(t, (t0 + t1)**0.5)
+            F[i], pval = stats.ttest_ind(class0, class1, equal_var=False)
     else:
         print('y should be guaranteed to a binary class vector')
         exit(0)
@@ -51,4 +51,56 @@ def feature_ranking(F):
     """
     idx = np.argsort(F)
     return idx[::-1]
+
+
+class TTest(SelectorMixin, MetaEstimatorMixin, BaseEstimator):
+    """ReliefF feature selection algorithm."""
+
+    def __init__(self, n_features_to_select=0.05, memory=None):
+        """Initialize mRMR.
+        Parameters
+        ----------
+        n_features_to_select : int or float
+            Number of features to select.
+        memory : Memory
+            Memory for caching the results of reliefF.
+        """
+        self.n_features_to_select = n_features_to_select
+        self.memory = memory
+
+    def fit(self, X, y):
+        """
+        Fit the Ttest.
+        Parameters
+        ----------
+        X : {np.ndarray} of shape (n_samples, n_features)
+            The training samples.
+        y : {numpy array} of shape (n_samples,)
+            The training labels
+        Returns
+        -------
+        self : object
+            Fitted estimator.
+        """
+        n_features = X.shape[1]
+        if isinstance(self.n_features_to_select, float):
+            n_features_to_select = round(self.n_features_to_select * n_features)
+        else:
+            n_features_to_select = self.n_features_to_select
+
+        if self.memory is not None:
+            ttest = self.memory.cache(t_score)
+            scores_ = ttest(X, y)
+        else:
+            scores_ = t_score(X, y)
+
+        indices = feature_ranking(scores_)[:n_features_to_select]
+
+        self.scores_ = scores_
+        self.support_ = np.zeros(n_features, dtype=bool)
+        self.support_[indices] = True
+        return self
+
+    def _get_support_mask(self):
+        return self.support_
 
